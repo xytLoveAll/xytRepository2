0ede522f82e650a2323d5480dba0beb724f9e10e
xyt <3214732859@qq.com>
2021-09-06 17:28:42 +0800

    Ssssssssssssssssssssssssssssssssss000000000000000000000000

---
+++ a/src/SSSSSSSSSSS.java
@@ -0,0 +1,10 @@
+import java.util.Map;
+
+/**
+ * @date 2021/9/6-17:27
+ */
+public class SSSSSSSSSSS {
+    public static void main(String[] args) {
+        System.out.println("ssssssssssssssssssssssssssssssssssssssss");
+    }
+}
